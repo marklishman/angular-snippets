3467ae348927e14c208f64fb0a82dbe62db868bf
marklishman <mark.lishman@gmail.com>
2018-01-29 20:11:03 +0000

    Removed router menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div>
-      <a routerLink="/array-gen" routerLinkActive="disabled">Array Gen</a> |
-      <a routerLink="/enum-iter" routerLinkActive="disabled">Enum Iterator</a> |
-    </div>
-
     <router-outlet></router-outlet>
   `,
 })
