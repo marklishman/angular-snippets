968dfc759e882583237e171a2df71828a71ccd34
marklishman <mark.lishman@gmail.com>
2018-01-31 20:27:52 +0000

    Optional menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   template: `
+    <div *ngIf="dev">
+      <a routerLink="/array-gen" routerLinkActive="disabled">Array Gen</a> |
+      <a routerLink="/enum-iter" routerLinkActive="disabled">Enum Iteration</a>
+    </div>
     <router-outlet></router-outlet>
-  `,
+  `
 })
 export class AppComponent {
+  dev = !environment.production;
 }
